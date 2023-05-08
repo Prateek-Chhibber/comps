c3e7b2285559a453e1d5044852b1737aee07109e
prateekchhibber <0392pc@gmail.com>
2023-05-08 23:30:00 +0530

    Passed boolean props and added prop-types library

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,14 @@
-function Button({ children }){
-    return <button>{children}</button>
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) {
+  return <button>{children}</button>;
 }
 
-export default Button
+export default Button;
