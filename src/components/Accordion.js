4a9dc5cead85fdc4d26a02c526b745110c1be038
prateekchhibber <0392pc@gmail.com>
2023-07-25 00:04:41 +0530

    Toggling Panel

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,9 +2,13 @@ import { useState } from "react"
 import {GoChevronDown, GoChevronLeft} from "react-icons/go"
 
 function Accordion ({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(0)
+    const [expandedIndex, setExpandedIndex] = useState(-1)
     const handleClick = (nextIndex) => {
-        setExpandedIndex(nextIndex)
+        if(expandedIndex === nextIndex){
+            setExpandedIndex(-1)
+        }else{
+            setExpandedIndex(nextIndex)
+        }
     }
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex
