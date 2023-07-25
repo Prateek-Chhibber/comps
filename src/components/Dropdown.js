d9f86fa14562996d7bf6c5036f2e6b02d8b831e5
prateekchhibber <0392pc@gmail.com>
2023-07-25 19:19:20 +0530

    Existence Helper Checker

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -16,13 +16,8 @@ function Dropdown({ options, selection, onSelect }){
         return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>;
       });
 
-      let content = 'Select...'
-      if(selection){
-        content = selection.label
-      }
-
     return (<div>
-        <div onClick={handleClick}>{content}</div>
+        <div onClick={handleClick}>{selection?.label || 'Select...'}</div>
         {isOpen && <div>{renderedOptions}</div>}
     </div>)
 }
