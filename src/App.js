fedb122a44e598c17ea32b597ddacb6613b824a1
prateekchhibber <0392pc@gmail.com>
2023-07-25 17:24:23 +0530

    Component Creation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-// import Button from "./components/Button";
-// import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
-// import { VscWarning } from "react-icons/vsc";
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown"
 
 function App() {
-  const items = [
-    {
-      id: '1',
-      label: 'Can i use React on a project?',
-      content: 'yes'
-    },
-    {
-      id: '2',
-      label: 'Can i use Javascript on a project?',
-      content: 'Yes'
-    },
-    {
-      id: '3',
-      label: 'Can i use CSS on a project?',
-      content: 'Yes'
-    },
-    {
-      id: '4',
-      label: 'Can i use EcmaScript on a project?',
-      content: 'Yes'
-    },
-    {
-      id: '5',
-      label: 'Can i use Java on a project?',
-      content: 'No'
-    },
-    {
-      id: '6',
-      label: 'Can i use Docker on a project?',
-      content: 'No'
-    }
-  ]
-  return <Accordion items={items} />
+  const options = [
+    { label: 'Red', value: 'red'},
+    { label: 'Green', value: 'green'},
+    { label: 'Blue', value: 'blue'},
+  ];
+
+  return <Dropdown options={options} />
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,39 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+  const items = [
+    {
+      id: '1',
+      label: 'Can i use React on a project?',
+      content: 'yes'
+    },
+    {
+      id: '2',
+      label: 'Can i use Javascript on a project?',
+      content: 'Yes'
+    },
+    {
+      id: '3',
+      label: 'Can i use CSS on a project?',
+      content: 'Yes'
+    },
+    {
+      id: '4',
+      label: 'Can i use EcmaScript on a project?',
+      content: 'Yes'
+    },
+    {
+      id: '5',
+      label: 'Can i use Java on a project?',
+      content: 'No'
+    },
+    {
+      id: '6',
+      label: 'Can i use Docker on a project?',
+      content: 'No'
+    }
+  ]
+  return <Accordion items={items} />
+}
+
+export default AccordionPage;
