b7c6ab36502be0f8f80321b141aaf10a0a1f68fb
prateekchhibber <0392pc@gmail.com>
2023-07-29 18:34:38 +0530

    Adding styling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,25 +1,46 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 
-function Dropdown({ options, value, onChange }){
-    const [isOpen, setIsOpen] = useState(false)
+function Dropdown({ options, value, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleClick = () => {
-        setIsOpen(!isOpen);
-      };
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const handleOptionClick = (option) => {
-        setIsOpen(false)
-        onChange(option)
-    }
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    onChange(option);
+  };
 
-    const renderedOptions = options.map((option) => {
-        return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>;
-      });
+  const renderedOptions = options.map((option) => {
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
+    );
+  });
 
-    return (<div>
-        <div onClick={handleClick}>{value?.label || 'Select...'}</div>
-        {isOpen && <div>{renderedOptions}</div>}
-    </div>)
+  return (
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="abcolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Dropdown
+export default Dropdown;
